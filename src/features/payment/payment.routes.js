2976465fefd61052d797aa285d8ff285d63a6e86
SpectraX07 <spectraxcodes07@gmail.com>
2026-06-08 21:06:33 +0530

    response returned from test webhook

--- src/features/payment/payment.routes.js
+++ src/features/payment/payment.routes.js
@@ -8,6 +8,9 @@ router.post('/create', userAuth, paymentController.createOrder);
 router.post('/razorpay-webhook', paymentController.razorpayWebhook);
 router.get('/razorpay-webhook', async (req, res) => {
   console.log('Received GET request on Razorpay webhook endpoint');
+  return res
+    .status(200)
+    .json({ success: true, message: 'GET request received' });
 }); // For testing webhook with GET requests
 
 export default router;
